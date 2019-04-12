bc8bed0a47e6a98aaf4d66acb4bd6fc90bd5af86
“ccaiqiandu <1654520765@qq.com>
2019-04-12 19:16:08 +0800

    添加user对象

---
+++ src/main/java/com/jk/model/User.java
@@ -0,0 +1,13 @@
+package com.jk.model;
+
+/**
+ * @author ：mmzs
+ * @date ：Created in 2019/4/12 19:14
+ * @description：
+ * @modified By：
+ * @version: $
+ */
+public class User {
+    private String aa;
+    private String bb;
+}
